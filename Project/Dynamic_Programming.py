63a962f26d9e1550a48820c47b037b3903a7756b
Raj Patel <58700508+rajpatel1904@users.noreply.github.com>
2022-12-16 23:26:31 -0800

    Add files via upload

---
+++ Project/Dynamic_Programming.py
@@ -0,0 +1,35 @@
+def dynamic_method(g, W, P):
+    table = [[0 for w in range(g + 1)] for j in range(len(W))]
+
+    for j in range(len(W)):
+        for w in range(g + 1):
+            if w == 0:
+                table[j][w] = 0
+
+            elif W[j] > w:
+                table[j][w] = table[j - 1][w]
+
+            else:
+                table[j][w] = max(table[j - 1][w], P[j] + table[j - 1][w - W[j]])
+
+    res = table[len(W) - 1][g] 
+    
+    w = g
+    num_pieces = 0
+    
+    for i in range(len(W) - 1):
+        if res <= 0:
+            break
+        if res == table[i][w]:
+            continue
+        else:
+            numPieces += 1
+            res = res + P[i]
+            i = w + W[i]
+    return num_pieces
+g= 50
+W= [1,5,10,15]
+P= [100,600,1000,1400]
+print(dynamic_method(g,W,P))
+
+
